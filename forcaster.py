f9c32f08670e793e31663bf23681bd604ab541cf
Nolhan Bertille <nbertille01@gmail.com>
2017-01-15 19:42:25 +0100

    Create class forcaster

--- forcaster.py
+++ forcaster.py
@@ -4,16 +4,26 @@ import os
 import httplib
 
 MYKEY="2914e9db205477351929329d6eae4772"
-URI="/data/2.5/weather?q=Geneve"
-URL=URI+'&APPID='+MYKEY+'&mode=xml'
+URI="/data/2.5/weather?q="
+HOSTNAME="api.openweathermap.org"
 
+class Forcaster:
+	def __init__(self, city):
+		self.city = city
 
-connection = httplib.HTTPConnection('api.openweathermap.org')
-connection.request("GET", URL)
-response = connection.getresponse()
-data = response.read()
+	def searchForcast(self):
+		connection = httplib.HTTPConnection(HOSTNAME)
 
-print response.status
-print data
+		URL = URI + self.city + '&APPID=' + MYKEY + '&mode=xml' 
+		
+		connection.request("GET", URL)
+		response = connection.getresponse()
+		data = response.read()
 
-connection.close()
+		print response.status
+		print data
+
+		connection.close()
+
+forcastGVA = Forcaster("Paris")
+forcastGVA.searchForcast()
