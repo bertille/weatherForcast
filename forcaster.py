4f5dd4853cd7a6c32dd70bf5f161193c68181211
Nolhan Bertille <nbertille01@gmail.com>
2017-01-15 16:20:55 +0100

    Send a request et receive a proper response

--- forcaster.py
+++ forcaster.py
@@ -1,5 +1,19 @@
-!# /usr/bin/python
+#!/usr/bin/python
 
 import os
+import httplib
 
-#for ...
+MYKEY="2914e9db205477351929329d6eae4772"
+URI="/data/2.5/weather?q=Geneva"
+URL=URI+'&APPID='+MYKEY+'&mode=xml'
+
+
+connection = httplib.HTTPConnection('api.openweathermap.org')
+connection.request("GET", URL)
+response = connection.getresponse()
+data = response.read()
+
+print response.status
+print data
+
+connection.close()
