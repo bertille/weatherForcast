18dcbde68dd5d04ecc6d18bb399d0f04655f10bb
Nolhan Bertille <nbertille01@gmail.com>
2017-01-15 19:49:02 +0100

    Add check on http response

--- forcaster.py
+++ forcaster.py
@@ -11,7 +11,7 @@ class Forcaster:
 	def __init__(self, city):
 		self.city = city
 
-	def searchForcast(self):
+	def search(self):
 		connection = httplib.HTTPConnection(HOSTNAME)
 
 		URL = URI + self.city + '&APPID=' + MYKEY + '&mode=xml' 
@@ -19,11 +19,15 @@ class Forcaster:
 		connection.request("GET", URL)
 		response = connection.getresponse()
 		data = response.read()
+	
+		if response.status == 200:
+			print(data)
+			return data
+			connection.close()
+		else:
+			print(response)
+			connection.close()
 
-		print response.status
-		print data
-
-		connection.close()
 
 forcastGVA = Forcaster("Paris")
-forcastGVA.searchForcast()
+forcastGVA.search()
