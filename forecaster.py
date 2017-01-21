c28045d7468b43ff4b963bb13586b6a217dca5a7
Nolhan Bertille <nbertille01@gmail.com>
2017-01-21 13:46:53 +0100

    Remove useless imports

--- forecaster.py
+++ forecaster.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python
 
-import os
 import httplib
 
+# Variables
 MYKEY="2914e9db205477351929329d6eae4772"
 URI="/data/2.5/weather?q="
 HOSTNAME="api.openweathermap.org"
+
 
 class Forecaster:
 	def __init__(self, city):
 		self.city = city
 
 	def search(self):
-		connection = httplib.HTTPConnection(HOSTNAME)
+		connection=httplib.HTTPConnection(HOSTNAME)
+		CITY=self.city
+		URL=URI+CITY+'&APPID='+MYKEY+'&mode=xml'
 
-		URL = URI + self.city + '&APPID=' + MYKEY + '&mode=xml' 
-		
 		connection.request("GET", URL)
-		response = connection.getresponse()
-		data = response.read()
+		response=connection.getresponse()
+		data=response.read()
 	
 		if response.status == 200:
 			print(data)
@@ -28,6 +29,8 @@ class Forecaster:
 			print(response)
 			connection.close()
 
+	def getCity(self):
+		return self.city
 
-forecastGVA = Forecaster("Paris")
+forecastGVA=Forecaster("Paris")
 forecastGVA.search()
